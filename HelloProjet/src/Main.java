a09d54fb28d4a19930ef3cf657c73e9262cf1aa3
lisus2000 <lisus2000@163.com>
2020-01-16 15:21:34 +0800

    代码修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,10 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("hello,World");
+    }
+    @Test
+    public void test2(){
+
     }
 }
